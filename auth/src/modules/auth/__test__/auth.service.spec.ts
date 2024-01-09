d84f0976d80293fb638f96508d5bc95be038cec0
Suraj <surajchan68@gmail.com>
2024-01-10 01:16:18 +0545

    feat: test case of unsuccess api call

--- auth/src/modules/auth/__test__/auth.service.spec.ts
+++ auth/src/modules/auth/__test__/auth.service.spec.ts
@@ -70,13 +70,9 @@ describe("Auth service", () => {
       await expect(authService.signup(payload)).rejects.toThrow(AUTH_MESSAGE_CONSTANT.UNABLE_SIGNUP_USER);
     });
 
-    it("Returns 201, when user created successfully", async () => {
-      jest.spyOn(Auth, "findOne").mockResolvedValueOnce(null); // Simulating that email is not taken
-      jest.spyOn(Auth, "findOne").mockResolvedValueOnce(null); // Simulating that username is not taken
-
+    it("Returns 400, when unsuccessfully API call and delete the user", async () => {
+      jest.spyOn(Auth, "findOne").mockResolvedValueOnce(null).mockResolvedValueOnce(null); // Simulating that email and username are not taken
       jest.spyOn(BcryptHelper.prototype, "generateHashPassword").mockImplementation(mockGenerateHashPassword);
-      (BcryptHelper.prototype.generateHashPassword as jest.Mock).mockResolvedValue("hashedPassword");
-
       const userMockPayload = {
         _id: new mongoose.Types.ObjectId().toHexString(),
         email: payload.email,
@@ -88,6 +84,53 @@ describe("Auth service", () => {
       };
       (Auth.create as jest.Mock).mockResolvedValueOnce([userMockPayload] as IAuthDocument[]);
 
+      // (axios.post as jest.Mock).mockRejectedValueOnce({ message: "API Error" });
+      (axios.post as jest.Mock).mockRejectedValueOnce({
+        code: "ECONNREFUSED",
+        message: "Connection refused",
+      });
+      (Auth.findByIdAndDelete as jest.Mock).mockResolvedValueOnce(null);
+
+      const user = await authService.signup(payload);
+
+      await expect(authService.signup(payload)).rejects.toThrow(AUTH_MESSAGE_CONSTANT.UNABLE_SIGNUP_USER);
+      expect(Auth.findOne).toHaveBeenCalledTimes(2);
+      expect(Auth.create).toHaveBeenCalledWith({
+        password: "hashedPassword",
+        email: "devlop@yopmail.com",
+        username: "devlop",
+      });
+      expect(axios.post).toHaveBeenCalledWith(
+        "http://localhost:4000/api/v1/users",
+        expect.objectContaining({
+          firstName: payload.firstName,
+          lastName: payload.lastName,
+          email: user.email,
+          username: user.username,
+          address: payload.address,
+        })
+      );
+      expect(Auth.findByIdAndDelete).toHaveBeenCalledWith(new mongoose.Types.ObjectId().toHexString());
+    });
+
+    it("Returns 201, when user created successfully", async () => {
+      jest.spyOn(Auth, "findOne").mockResolvedValueOnce(null); // Simulating that email is not taken
+      jest.spyOn(Auth, "findOne").mockResolvedValueOnce(null); // Simulating that username is not taken
+
+      // jest.spyOn(BcryptHelper.prototype, "generateHashPassword").mockImplementation(mockGenerateHashPassword);
+      (BcryptHelper.prototype.generateHashPassword as jest.Mock).mockResolvedValue("hashedPassword");
+
+      const userMockPayload = {
+        _id: new mongoose.Types.ObjectId().toHexString(),
+        email: payload.email,
+        username: payload.username,
+        password: "hashedPassword",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        __v: 0,
+      };
+
+      (Auth.create as jest.Mock).mockResolvedValueOnce([userMockPayload] as IAuthDocument[]);
       (axios.post as jest.Mock).mockResolvedValueOnce({ data: { success: true } });
 
       const user = await authService.signup(payload);
@@ -107,11 +150,12 @@ describe("Auth service", () => {
         expect.objectContaining({
           firstName: payload.firstName,
           lastName: payload.lastName,
-          email: "devlop@yopmail.com",
-          username: "devlop",
+          email: user.email,
+          username: user.username,
           address: payload.address,
         })
       );
+
       expect(Auth.findByIdAndDelete).not.toHaveBeenCalled();
     });
   });
